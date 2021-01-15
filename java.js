4b68170c1bb4336353723288e194d63abf7ad995
AsadUllah <ahmadasad901@gmail.com>
2021-01-15 17:30:37 +0500

    made some changes in both index.html and java.js

--- java.js
+++ java.js
@@ -1,7 +1,9 @@
+console.log('js loaded');
 var firstName = 'Asad'
 var secondName = 'Ullah'
-console.log('My name is',firstName);
+console.log('My name is',firstName, secondName);
 var urdu = 90
 var english = 70
 var sum = urdu + english
 console.log('Total Marks =',sum);
+// alert('Are you sure you want to close?');
